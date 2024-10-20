55b5e6ab72ca7a3fdb8981dba0839173eec2b907
TheKingXion <luis.gonzalez.oyarce@alumnos.cmch.maristas.cl>
2024-10-19 22:09:31 -0300

    Nuevas cosas y Orden

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-x
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/select-language')
+def select_language():
+    return render_template('select_language.html')
+
+@app.route('/dictionary-usage')
+def dictionary_usage():
+    return render_template('dictionary_usage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
