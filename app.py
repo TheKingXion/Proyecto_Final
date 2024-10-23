0661b6b5ba5db20e2abe8def2165f9692cf89a9f
TheKingXion <luis.gonzalez.oyarce@alumnos.cmch.maristas.cl>
2024-10-23 09:31:35 -0300

    Nuevos cambios y fix

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/developers')
+def developers():
+    return render_template('developers.html')
+
 @app.route('/select-language')
 def select_language():
     return render_template('select_language.html')
