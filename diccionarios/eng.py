385f86e9cb095949756a549b74b13d6861856e84
TheKingXion <luis.gonzalez.oyarce@alumnos.cmch.maristas.cl>
2024-11-06 15:02:25 -0300

    test

--- diccionarios/eng.py
+++ diccionarios/eng.py
@@ -1,6 +1,5 @@
 #vicho
 diccionario_eng = {
-<<<<<<< HEAD
 #1
     {
     "meaning": "",
@@ -601,99 +600,4 @@ diccionario_eng = {
     "use": "",
     "example": ""
 },
-=======
-    #Vicho
-    "a": {
-        # Aquí puedes agregar funciones que comiencen con la letra A
-    },
-    "b": {
-        # Aquí puedes agregar funciones que comiencen con la letra B
-    },
-    "c": {
-        # Aquí puedes agregar funciones que comiencen con la letra C
-    },
-    "d": {
-        # Aquí puedes agregar funciones que comiencen con la letra D
-    },
-    "e": {
-        # Aquí puedes agregar funciones que comiencen con la letra E
-    },
-    "f": {
-        # Aquí puedes agregar funciones que comiencen con la letra F
-    },
-    "g": {
-        # Aquí puedes agregar funciones que comiencen con la letra G
-    },
-    "h": {
-        # Aquí puedes agregar funciones que comiencen con la letra H
-    },
-    "i": {
-        # Aqui puedes agregar funciones que comiencen con la letra I
-    },
-    "j": {
-        # Aquí puedes agregar funciones que comiencen con la letra J
-    },
-    "k": {
-        # Aquí puedes agregar funciones que comiencen con la letra K
-    },
-    "l": {
-        # Aqui puedes agregar funciones que comiencen con la letra L
-    },
-    "m": {
-        # Aquí puedes agregar funciones que comiencen con la letra M
-    },
-    "n": {
-        # Aquí puedes agregar funciones que comiencen con la letra N
-    },
-    "o": {
-        # Aquí puedes agregar funciones que comiencen con la letra O
-    },
-    "p": {
-        # Aquí puedes agregar funciones que comiencen con la letra P
-    },
-    "q": {
-        # Aquí puedes agregar funciones que comiencen con la letra Q
-    },
-    "r": {
-        "range": {
-        "significado": "Returns a sequence of numbers, ussualy in loops.",
-        "uso": "To create a range of numbers that can be iterated",
-        "ejemplo": '''for i in range(5):
-            print(i)'''
-        },
-    },
-    "s": {
-        "str": {
-        "significado": "make a value into a text chain",
-        "uso": " to make a value like a text",
-        "ejemplo":''' # number
-        number = 123
-        number_like_str = str(number)
-        print(type(number_like_str))  # exit: <class 'str'>
-        print(number_like_str)        # exit: '123'''
-        },
-    },
-    
-    "t": {
-        # Aquí puedes agregar funciones que comiencen con la letra T
-    },
-    "u": {
-        # Aquí puedes agregar funciones que comiencen con la letra U
-    },
-    "v": {
-        # Aquí puedes agregar funciones que comiencen con la letra V
-    },
-    "w": {
-        # Aquí puedes agregar funciones que comiencen con la letra W
-    },
-    "x": {
-        # Aquí puedes agregar funciones que comiencen con la letra X
-    },
-    "y": {
-        # Aquí puedes agregar funciones que comiencen con la letra Y
-    },
-    "z": {
-        # Aquí puedes agregar funciones que comiencen con la letra Z
-    },
->>>>>>> 1e2839fa8525331de705f6e66fac148472c406b6
 }
