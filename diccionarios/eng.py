ba476b8c2eae9738f5f6e7b08363d0b3f0e197e7
Vicenticoo <vicente.soto.torres@alumnos.cmch.maristas.cl>
2024-11-06 13:01:35 -0300

    Lista en ingles Reworkeada

--- diccionarios/eng.py
+++ diccionarios/eng.py
@@ -1,603 +1,81 @@
 diccionario_eng = {
     #Vicho
-    #1
-    "1": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "a": {
+        # Aquí puedes agregar funciones que comiencen con la letra A
     },
-    #2
-    "2": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "b": {
+        # Aquí puedes agregar funciones que comiencen con la letra B
     },
-    #3
-    "3": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "c": {
+        # Aquí puedes agregar funciones que comiencen con la letra C
     },
-    #4
-    "4": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "d": {
+        # Aquí puedes agregar funciones que comiencen con la letra D
     },
-    #5
-    "5": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "e": {
+        # Aquí puedes agregar funciones que comiencen con la letra E
     },
-    #6
-    "6": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "f": {
+        # Aquí puedes agregar funciones que comiencen con la letra F
     },
-    #7
-    "7": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "g": {
+        # Aquí puedes agregar funciones que comiencen con la letra G
     },
-    #8
-    "8": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "h": {
+        # Aquí puedes agregar funciones que comiencen con la letra H
     },
-    #9
-    "9": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "i": {
+        # Aqui puedes agregar funciones que comiencen con la letra I
     },
-    #10
-    "10": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "j": {
+        # Aquí puedes agregar funciones que comiencen con la letra J
     },
-    #11
-    "11": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "k": {
+        # Aquí puedes agregar funciones que comiencen con la letra K
     },
-    #12
-    "12": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "l": {
+        # Aqui puedes agregar funciones que comiencen con la letra L
     },
-    #13
-    "13": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "m": {
+        # Aquí puedes agregar funciones que comiencen con la letra M
     },
-    #14
-    "14": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "n": {
+        # Aquí puedes agregar funciones que comiencen con la letra N
     },
-    #15
-    "15": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "o": {
+        # Aquí puedes agregar funciones que comiencen con la letra O
     },
-    #16
-    "16": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "p": {
+        # Aquí puedes agregar funciones que comiencen con la letra P
     },
-    #17
-    "17": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "q": {
+        # Aquí puedes agregar funciones que comiencen con la letra Q
     },
-    #18
-    "18": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "r": {
+        # Aquí puedes agregar funciones que comiencen con la letra R
     },
-    #19
-    "19": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "s": {
+        # Aquí puedes agregar funciones que comiencen con la letra S
     },
-    #20
-    "20": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "t": {
+        # Aquí puedes agregar funciones que comiencen con la letra T
     },
-    #21
-    "21": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "u": {
+        # Aquí puedes agregar funciones que comiencen con la letra U
     },
-    #22
-    "22": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "v": {
+        # Aquí puedes agregar funciones que comiencen con la letra V
     },
-    #23
-    "23": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "w": {
+        # Aquí puedes agregar funciones que comiencen con la letra W
     },
-    #24
-    "24": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "x": {
+        # Aquí puedes agregar funciones que comiencen con la letra X
     },
-    #25
-    "25": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "y": {
+        # Aquí puedes agregar funciones que comiencen con la letra Y
     },
-    #26
-    "26": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
+    "z": {
+        # Aquí puedes agregar funciones que comiencen con la letra Z
     },
-    #27
-    "27": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #28
-    "28": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #29
-    "29": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #30
-    "30": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #31
-    "31": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #32
-    "32": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #33
-    "33": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #34
-    "34": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #35
-    "35": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #36
-    "36": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #37
-    "37": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #38
-    "38": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #39
-    "39": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #40
-    "40": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #41
-    "41": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #42
-    "42": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #43
-    "43": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #44
-    "44": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #45
-    "45": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #46
-    "46": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #47
-    "47": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #48
-    "48": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #49
-    "49": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #50
-    "50": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #51
-    "51": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #52
-    "52": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #53
-    "53": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #54
-    "54": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #55
-    "55": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #56
-    "56": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #57
-    "57": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #58
-    "58": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #59
-    "59": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #60
-    "60": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #61
-    "61": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #62
-    "62": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #63
-    "63": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #64
-    "64": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #65
-    "65": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #66
-    "66": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #67
-    "67": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #68
-    "68": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #69
-    "69": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #70
-    "70": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #71
-    "71": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #72
-    "72": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #73
-    "73": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #74
-    "74": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #75
-    "75": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #76
-    "76": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #77
-    "77": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #78
-    "78": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #79
-    "79": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #80
-    "80": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #81
-    "81": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #82
-    "82": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #83
-    "83": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #84
-    "84": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #85
-    "85": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #86
-    "86": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #87
-    "87": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #88
-    "88": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #89
-    "89": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #90
-    "90": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #91
-    "91": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #92
-    "92": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #93
-    "93": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #94
-    "94": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #95
-    "95": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #96
-    "96": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #97
-    "97": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #98
-    "98": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #99
-    "99": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    },
-    #100
-    "100": {
-        "significado": "",
-        "uso": "",
-        "ejemplo": ""
-    }
 }
