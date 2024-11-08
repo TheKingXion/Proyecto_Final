4308f5276d1db1a9296e7587ad827b13996f2fed
Vicenticoo <vicente.soto.torres@alumnos.cmch.maristas.cl>
2024-11-08 10:19:44 -0300

    Mas conceptos traducidos en el diccionario eng

--- diccionarios/eng.py
+++ diccionarios/eng.py
@@ -1,7 +1,18 @@
 #vicho
 diccionario_eng = {
     "a": {
-        # Aquí puedes agregar funciones que comiencen con la letra A
+        "any": {
+            "significado": ": Returns True if at least one of the elements in an iterable is true",
+            "uso": "Verify if a condition is a true iterable",
+            "ejemplo": '''values = [0, False, 2]
+            print(any(values))  # True'''
+        },
+        "all": {
+            "significado": "Return True if all elements are true iterable",
+            "uso": "Verifi is all contitions are true iterable",
+            "ejemplo": '''values = [1, 2, 3]
+            print(all(values))  # True'''
+        },
     },
     "b": {
         "break": {
@@ -15,16 +26,52 @@ diccionario_eng = {
         },
     },
     "c": {
-        # Aquí puedes agregar funciones que comiencen con la letra C
+        "_constains_": {
+            "significado": "Defines how to check if one object contains another (for the in operator)",
+            "uso": "Customize the behavior of the in operator when used with an object of a user-defined class",
+            "ejemplo": '''class Box:
+            def __init__(self, elements):
+            self.elements = elements
+            def __contains__(self, item):
+            return item in self.elements
+            box = Box([1, 2, 3])
+            print(2 in box)  # True'''
+        },
+        "count": {
+            "significado":"Returns the number of times an item appears in a list or string",
+            "uso": "Count occurrences of a value in a collection",
+            "ejemplo": '''list = [1, 2, 2, 3, 2, 4]
+            print(list.count(2))  # 3'''
+        },
+     },
+     "clear": {
+            "significado":" Delete all from a list or dictionary",
+            "uso": "Empty the contents of a collection",
+            "ejemplo": '''list = [1, 2, 3]
+            list.clear()
+            print(list)  # []'''
     },
     "d": {
-        # Aquí puedes agregar funciones que comiencen con la letra D
+        "del": {
+            "significado": "Remove a object or variable",
+            "uso": "Delete a object, variable or element from a colletion",
+            "ejemplo": '''list = [1, 2, 3]
+            del list[0]
+            print(list)  # [2, 3]'''
+        },
     },
     "e": {
         # Aquí puedes agregar funciones que comiencen con la letra E
     },
     "f": {
-        # Aquí puedes agregar funciones que comiencen con la letra F
+        "format": {
+            "significado": "Allow to insert values into z string of text",
+            "uso": "It is used to format strings dynamically, inserting values in specific places",
+            "ejemplo": '''name = "Juan"
+            age = 30
+            message = "Hi, my name is {} and i am {} years old.".format(name, age)
+            print(message)  # "Hi, my name is Juan and i am 30 years old."'''
+        },
     },
     "g": {
         # Aquí puedes agregar funciones que comiencen con la letra G
@@ -51,7 +98,17 @@ diccionario_eng = {
             self.name = name
             self.age = age'''
         },
-
+        "index": {
+            "significado":"Returns the index of the first occurrence of a value in a list or string",
+            "uso": " Search the position of a element inside of a iterable",
+            " ejemplo": '''list = [10, 20, 30, 40]
+            print(list.index(30))  # 2'''
+        },
+        "int": {
+            "significado":"Turn a string into a whole number",
+            "uso": "It is used to convert numerical strings into integer values",
+            "ejemplo": '''int("10")  # 10'''
+    },
     },
     "j": {
         # Aquí puedes agregar funciones que comiencen con la letra J
