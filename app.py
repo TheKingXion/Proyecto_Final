99759da21e6d5c3e63ff0591bd6ccdde0e8d098a
King Xion <luis.gonzalez.oyarce@alumnos.cmch.maristas.cl>
2024-11-19 11:44:24 -0300

    Progreso

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from diccionarios import es, eng, fr, ita, por
+import requests
 
 app = Flask(__name__)
+
+diccionarios = {
+    "es": es.diccionario,
+    "en": eng.diccionario,
+    "fr": fr.diccionario,
+    "it": ita.diccionario,
+    "pt": por.diccionario,
+}
 
 @app.route('/')
 def index():
@@ -14,9 +24,14 @@ def developers():
 def select_language():
     return render_template('select_language.html')
 
-@app.route('/dictionary-usage')
+@app.route('/dictionary-usage', methods=['GET', 'POST'])
 def dictionary_usage():
-    return render_template('dictionary_usage.html')
+    # Captura el idioma seleccionado (por defecto español)
+    idioma = request.args.get('idioma', 'es')
+    # Obtén el diccionario correspondiente
+    diccionario = diccionarios.get(idioma, diccionarios["es"])
+    # Renderiza la plantilla y pasa los datos
+    return render_template('dictionary_usage.html', datos=diccionario, idioma=idioma)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
